160c1d4a839fee0afe2aeaceefe44c75b6859faa
anasarshad143-lang <anasarshad143@gmail.com>
2026-09-04 16:12:18 +0500

    Add superuser creation script to build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,11 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py loaddata datadump.json
+
+# Create superuser automatically if it doesn't exist
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'anasarshad143@gmail.com', 'Anas@0325')
+"
