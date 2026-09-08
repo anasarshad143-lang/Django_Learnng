cb9e2d6816db296008b2706af9de1783049f4dac
anasarshad143-lang <anasarshad143@gmail.com>
2026-09-08 20:47:30 +0500

    Fix email SMTP configuration to use SSL port 465

--- config/settings.py
+++ config/settings.py
@@ -140,14 +140,15 @@ STORAGES = {
 
 
 # Email Configuration
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 465))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'True') == 'True'
+EMAIL_TIMEOUT = 10  # Prevent infinite hangs on bad SMTP connections
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER or 'webmaster@localhost'
 
 
 LOGIN_URL = "/login/"
