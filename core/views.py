03a8de91790ff28340f52e2759b14175da17b7f1
anasarshad143-lang <anasarshad143@gmail.com>
2026-09-09 19:07:47 +0500

    Fix hardcoded sender email and DB config

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 import random
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
